48d1fb245101844cd4ce19b28148ce4c980893a8
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-02-26 19:36:19 +1000

    impr: add secret file exists check explicitly

--- src/shhh/__init__.py
+++ src/shhh/__init__.py
@@ -10,6 +10,14 @@ def get_secret(name: str) -> str:
 
     if filename_env_key in env:
         environment_secret_filepath: Path = Path(env[filename_env_key])
+        if (
+            not environment_secret_filepath.exists()
+            or not environment_secret_filepath.is_file()
+        ):
+            raise ValueError(
+                f"{filename_env_key} is provided, but that file not found (that path hidden for security purposes).\nDid you forget to provide that file via docker volumes, for example?"
+            )
+
         with open(environment_secret_filepath, "r") as secret_file:
             return secret_file.read()
 
